35856a0d0a4aaa986fc9cefbd719c89e1bae34ff
Imtiaz Zahir <imtiazzahir43@gmail.com>
2023-12-24 23:22:15 +0600

    fis all-students

--- app/api/all-students/route.ts
+++ app/api/all-students/route.ts
@@ -2,19 +2,41 @@ import { connectToDB } from "@/database/connect";
 import Student from "@/models/student";
 import { NextResponse } from "next/server";
 
-export async function GET(){
-    try {
-        await connectToDB();
-        const students=await Student.find().select(["uid","name","roll","registration","phone","father","fatherphone","mother","dist","upzila","session","room_no","student_id","is_admin","is_verified"])
-        return NextResponse.json({
-            status:"1",
-            message:"Data found",
-            data:students
-        })
-    } catch (error) {
-        return NextResponse.json({
-            status:"0",
-            message:"Something went wrong"
-        })
+export async function GET() {
+  try {
+    await connectToDB();
+    const students = await Student.find().select([
+      "uid",
+      "name",
+      "roll",
+      "registration",
+      "phone",
+      "father",
+      "fatherphone",
+      "mother",
+      "dist",
+      "upzila",
+      "session",
+      "room_no",
+      "student_id",
+      "is_admin",
+      "is_verified",
+    ]);
+    if (!students) {
+      return NextResponse.json({
+        status: "0",
+        message: "No data found",
+      });
     }
+    return NextResponse.json({
+      status: "1",
+      message: "Data found",
+      data: students,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      status: "0",
+      message: "Something went wrong",
+    });
+  }
 }
